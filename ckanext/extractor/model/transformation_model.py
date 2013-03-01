8f68fcb699d86aba0e76f442f76daa53f4cb8478
Unai Aguilera <gkalgan@gmail.com>
2013-03-01 12:13:34 +0100

    Transformation model updated.

--- ckanext/extractor/model/transformation_model.py
+++ ckanext/extractor/model/transformation_model.py
@@ -8,15 +8,15 @@ class Transformation(Base):
 
     package_name = Column(String, primary_key=True)
     filename = Column(String, nullable=False)
-    code = Column(LargeBinary, nullable=False)
+    data = Column(LargeBinary, nullable=False)
     timestamp = Column(DateTime, nullable=False)
     mainclass = Column(String, nullable=False)
     enabled = Column(Boolean, nullable=False)
 
-    def __init__(self, package_name, filename, code, timestamp, mainclass, enabled=True):
+    def __init__(self, package_name, filename=None, data=None, timestamp=None, mainclass=None, enabled=False):
         self.package_name = package_name
         self.filename = filename
-        self.code = code
+        self.data = data
         self.timestamp = timestamp
         self.mainclass = mainclass
         self.enabled = enabled
