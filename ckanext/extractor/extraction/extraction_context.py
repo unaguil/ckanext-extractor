120f9bedb0ca9afe395bae3966a2bc39004b2a61
Unai Aguilera <gkalgan@gmail.com>
2013-03-11 11:46:51 +0100

    Empty context was incorrectly set

--- ckanext/extractor/extraction/extraction_context.py
+++ ckanext/extractor/extraction/extraction_context.py
@@ -18,7 +18,7 @@ class ExtractionContext():
 		self.extraction = session.query(Extraction).filter_by(transformation_id=transformation.package_id).order_by(desc(Extraction.start_date)).first()
 
 		if self.extraction is None:
-			self.extraction = Extraction(datetime.now(), '', WORKING)
+			self.extraction = Extraction(datetime.now(), '{}', WORKING)
 			self.transformation.extractions.append(self.extraction)
 		else:
 			self.extraction = Extraction(datetime.now(), self.extraction.context, WORKING)
@@ -28,14 +28,16 @@ class ExtractionContext():
 		self.session.commit()
 
 	def update_context(self, new_context):
-		self.extraction.context = new_context
+		self.extraction.context = unicode(new_context)
 		session.merge(self.transformation)
 		session.commit()
 
 	def get_current_context(self):
-		return self.extraction.context
+		return eval(self.extraction.context)
 
-	def finish_ok(self, comment):
+	def finish_ok(self, comment, new_context):
+		self.update_context(new_context)
+
 		self.extraction.end_date = datetime.now()
 		self.extraction.transformation_status = OK
 		self.extraction.comment = comment
