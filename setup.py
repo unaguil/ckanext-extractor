6056872e80caccec74f653ef6aba57e9ca4a4531
Unai Aguilera <gkalgan@gmail.com>
2013-07-01 12:54:11 +0200

    Celery version dependency removed.

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     include_package_data=True,
     zip_safe=False,
     install_requires=[
-        'celery==2.4.2',],
+        'celery',],
     entry_points=\
     """
     [ckan.plugins]
