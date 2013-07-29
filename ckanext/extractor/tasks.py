7779e9394507138c81e6f8fa8654923d2634fd7d
Unai Aguilera <gkalgan@gmail.com>
2013-07-29 10:33:44 +0200

    Duplicate task execution checked and working

--- ckanext/extractor/tasks.py
+++ ckanext/extractor/tasks.py
@@ -40,6 +40,7 @@ session = sessionmaker(bind = engine)()
 @celery.task(name="extractor.perform_extraction")
 def perform_extraction(package_id, mainclass):
     full_task_id = perform_extraction.name + '_' + package_id
+    
     if not is_running(full_task_id):
         add_task(full_task_id)
         
@@ -61,7 +62,7 @@ def perform_extraction(package_id, mainclass):
             context.finish_error(comment)
             log.info(comment)
     else:
-        print 'Task is already running'
+        print 'Extraction task is already running %s' % full_task_id
 
 @celery.task(name="extractor.install_dependencies")
 def install_dependencies(required):
@@ -84,14 +85,16 @@ def is_running(task_name):
 def add_task(task_name):
     running_task = RunningTask(task_name, datetime.now())
     session.add(running_task)
+    session.commit()
+    
+    print 'Saved extraction for package_id %s' % task_name
     
 def clear_running_tasks():
+    print 'Cleaning old running tasks'
     session.query(RunningTask).delete()
 
 @periodic_task(run_every=timedelta(seconds=int(RUN_EVERY_SECONDS)))
-def launch_transformations():
-    clear_running_tasks()
-    
+def launch_transformations():    
     log.info('Checking transformation crontabs')
     transformations = session.query(Transformation).all()
 
@@ -100,3 +103,5 @@ def launch_transformations():
             mainclass, _ = get_config_data(t.output_dir)
             celery.send_task("extractor.perform_extraction",
                 args=[t.package_id, mainclass], task_id=str(uuid.uuid4()))
+
+clear_running_tasks()
